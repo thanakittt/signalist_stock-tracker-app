abd1ca60d097588c1420f5dc1778dce0b48cb71c
root <root@LAPTOP-RM1K64U6.localdomain>
2025-10-31 16:52:52 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Button</Button>
+      
+    </div>
+  );
+};
+
+export default Home;
