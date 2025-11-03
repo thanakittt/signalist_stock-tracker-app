1c95ce9f1be09ceb2ec0d44b84e37c52ac50e2fa
Thanakit2023 <150004716+Thanakit2023@users.noreply.github.com>
2025-11-03 14:08:13 +0700

    implement database setup

---
+++ database/mongoose.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please provide a MONGODB_URI in the environment variables"
+    );
+  }
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+};
