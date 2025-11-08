4af7f228bb46229e30244c3d9cb8c2460060c8a0
Thanakit2023 <150004716+Thanakit2023@users.noreply.github.com>
2025-11-08 20:31:47 +0700

    Implement stocks search

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
-'use client';
-
+ 
 import Image from "next/image";
 import Link from "next/link";
 import NavItems from "@/components/NavItems";
 import UserDropdown from "@/components/UserDropdown";
+import { searchStocks } from "@/lib/actions/finnhub.actions";
 
-const Header = ({ user }: {user: User}) => {
+const Header = async ({ user }: {user: User}) => {
+  const initialStocks = await searchStocks()
   return (
     <header className="sticky top-0 header">
       <div className="container header-wrapper">
@@ -19,9 +20,9 @@ const Header = ({ user }: {user: User}) => {
           />
         </Link>
         <nav className="hidden sm:block">
-          <NavItems />
+          <NavItems initialStocks={initialStocks} />
         </nav>
-        <UserDropdown user={user} />
+        <UserDropdown user={user} initialStocks={initialStocks} />
       </div>
     </header>
   );
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -4,8 +4,13 @@ import Link from "next/link";
 import { NAV_ITEMS } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import { SearchCommand } from "@/SearchCommand";
 
-const NavItems = () => {
+const NavItems = ({
+  initialStocks,
+}: {
+  initialStocks: StockWithWatchlistStatus[];
+}) => {
   const pathname = usePathname();
 
   const isActive = (path: string) => {
@@ -15,19 +20,32 @@ const NavItems = () => {
 
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-      {NAV_ITEMS.map(({ label, href }) => (
-        <li key={label}>
-          <Link
-            href={href}
-            className={cn(
-              "hover:text-yellow-500 transition-colors",
-              isActive(href) ? "text-gray-100" : ""
-            )}
-          >
-            {label}
-          </Link>
-        </li>
-      ))}
+      {NAV_ITEMS.map(({ label, href }) => {
+        if (label === "Search")
+          return (
+            <li key="search-trigger">
+              <SearchCommand
+                renderAs="text"
+                label="Search"
+                initialStocks={initialStocks}
+              />
+            </li>
+          );
+
+        return (
+          <li key={label}>
+            <Link
+              href={href}
+              className={cn(
+                "hover:text-yellow-500 transition-colors",
+                isActive(href) ? "text-gray-100" : ""
+              )}
+            >
+              {label}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
